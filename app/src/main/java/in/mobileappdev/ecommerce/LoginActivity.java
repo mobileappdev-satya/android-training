b222a4c391834ba6cad53361fde4d464d5af432f
Satya Attili <satya@mobileappdev.in>
2017-08-02 00:21:06 +0530

    Lifecycle methods added

--- app/src/main/java/in/mobileappdev/ecommerce/LoginActivity.java
+++ app/src/main/java/in/mobileappdev/ecommerce/LoginActivity.java
@@ -3,18 +3,20 @@ package in.mobileappdev.ecommerce;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Log.d(TAG, "LifeCycle - onCreate");
         //registering views from XML
         Button buttonSignIn = (Button) findViewById(R.id.btnSignIn);
         TextView textCreateAccount = (TextView) findViewById(R.id.txt_create_account);
@@ -25,6 +27,42 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         textCreateAccount.setOnClickListener(this);
         textForgotPassword.setOnClickListener(this);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "LifeCycle - onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "LifeCycle - onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "LifeCycle - onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "LifeCycle - onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "LifeCycle - onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "LifeCycle - onDestroy");
     }
 
     @Override
