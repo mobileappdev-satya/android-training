41206d87b131a33ebcf8f96d4a9b48c731013ea2
Satya Attili <satya@mobileappdev.in>
2017-08-20 10:00:50 +0530

    SQLIte for Items added

--- app/src/main/java/in/mobileappdev/ecommerce/HomeActivity.java
+++ app/src/main/java/in/mobileappdev/ecommerce/HomeActivity.java
@@ -5,29 +5,57 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import in.mobileappdev.ecommerce.db.SqliteDbHandler;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
+    private SqliteDbHandler sqliteDbHandler;
+    private String username;
+    private  TextView txtUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        sqliteDbHandler = new SqliteDbHandler(this, SqliteDbHandler.DB_NAME, null, 1);
+
         SharedPreferences sp = getSharedPreferences("in.mobileappdev.ecommerce",MODE_PRIVATE);
-        String username = sp.getString("username", "UserName");
+        username = sp.getString("username", "UserName");
 
-        TextView txtUserName = (TextView) findViewById(R.id.txt_user_name);
+        txtUserName = (TextView) findViewById(R.id.txt_user_name);
 
-        Intent intent  = getIntent();
-       // String username = intent.getStringExtra("usernam");
-        Log.d(TAG, "Recived Username : "+username);
-        if(username != null){
-            txtUserName.setText(username.toLowerCase());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int countOftheItems = sqliteDbHandler.getItemsCount();
+        txtUserName.setText("Hi "+username.toLowerCase()+"\n We have "+countOftheItems +" items for you.");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                startActivity(new Intent(HomeActivity.this, AddItemActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-
     }
 }
---
+++ app/src/main/java/in/mobileappdev/ecommerce/db/SqliteDbHandler.java
@@ -0,0 +1,60 @@
+package in.mobileappdev.ecommerce.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Techjini on 8/17/2017.
+ */
+
+public class SqliteDbHandler extends SQLiteOpenHelper{
+
+    public static final String DB_NAME = "ecommDB";
+    public static  final String TABLE_ITEMS = "ITEMS";
+    public static final String COL_ITEM_ID = "id";
+    public static final String COL_ITEM_NAME = "item_name";
+    public static final String COL_ITEM_DESC = "item_desc";
+    public static final String COL_ITEM_COST = "item_cost";
+    public static final String COL_ITEM_QUANTITY = "item_quantity";
+    private static final String TAG = "SqliteDbHandler";
+
+
+    public SqliteDbHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_ITEMS_TABLE = "CREATE TABLE "+TABLE_ITEMS+" ( "+COL_ITEM_ID+" INT, "+COL_ITEM_NAME+" VARCHAR(20) , "+COL_ITEM_DESC+" VARCHAR(50), "+COL_ITEM_COST+" INT, "+COL_ITEM_QUANTITY+" INT);";
+        Log.d(TAG, "CREATE_ITEMS_TABLE = "+CREATE_ITEMS_TABLE);
+        db.execSQL(CREATE_ITEMS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //don't worry about this for now
+    }
+
+
+    public void insertItem(String name, String desc, int cost, int qty){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ITEM_NAME, name);
+        contentValues.put(COL_ITEM_DESC, desc);
+        contentValues.put(COL_ITEM_COST, cost);
+        contentValues.put(COL_ITEM_QUANTITY, qty);
+        getWritableDatabase().insert(TABLE_ITEMS, null, contentValues);
+    }
+
+    public int getItemsCount(){
+        String SELECT_TEMS = "SELECT * FROM "+TABLE_ITEMS+";";
+        Cursor cursor = getWritableDatabase().rawQuery(SELECT_TEMS, null);
+        int count  = cursor.getCount();
+        cursor.close();
+        return count;
+    }
+}
